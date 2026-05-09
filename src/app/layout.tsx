98e280b266af499bd5a66e5ab463e586ec6189c9
Niakimbo22 <niakimbo22@github.com>
2026-05-09 02:41:21 +0200

    fix: suppression particules (effet tâche au scroll)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import VersionChecker from "@/components/VersionChecker";
 import PageTransition from "@/components/PageTransition";
 import LuxuryCursor from "@/components/LuxuryCursor";
 import ScrollReveal from "@/components/ScrollReveal";
-import AmbientParticles from "@/components/AmbientParticles";
 
 const inter = Inter({
   variable: "--font-sans",
@@ -50,7 +49,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-luxury-black">
         <LuxuryCursor />
-        <AmbientParticles />
         <ScrollReveal />
         <VersionChecker />
         <PageTransition />
